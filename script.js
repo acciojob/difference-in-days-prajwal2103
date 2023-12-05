c2def9e01d2ac3e8bafda8acc24b33bd319e1fd2
prajwal2103 <137304916+prajwal2103@users.noreply.github.com>
2023-12-05 18:20:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 var dateDiffInDays = function (date1, date2) {
   //   write your code here
+	date1=Date.UTC(date1);
+	date2=Date.UTC(date2);
+	let diff=date1-date2;
+	diff=parseInt(diff/86400000);
+	return diff;
 };
 
 // Do not change the code below.
-const dateOne = prompt("Enter Start Date.");
-const dateTwo = prompt("Enter End Date.");
-alert(dateDiffInDays(dateOne, dateTwo));
+// const dateOne = prompt("Enter Start Date.");
+// const dateTwo = prompt("Enter End Date.");
+// alert(dateDiffInDays(dateOne, dateTwo));
